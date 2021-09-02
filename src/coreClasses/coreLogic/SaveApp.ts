22aa456de80b652966749994cd0e40a6b10fd85c
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-02 19:50:44 +1200

    Create SaveApp.ts
    Created
    
    Need to implement further

---
+++ src/coreClasses/coreLogic/SaveApp.ts
@@ -0,0 +1,20 @@
+import { AppEnvironment } from '../coreObjects/AppEnvironment';
+const ess = require('esserializer');
+
+class SaveApp {
+
+    private _appEnvironment : AppEnvironment;
+    // TODO fill up this array!
+    private requiredClasses = [];
+ 
+    constructor(appEnvironment : AppEnvironment) {
+        this._appEnvironment = appEnvironment;
+    }
+
+
+    private saveSession() : boolean {
+        // Returns boolean if session was successfully saved
+        let userName : string = this._appEnvironment.user.name;
+        return false;
+    }
+}
