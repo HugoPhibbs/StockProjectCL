61a5f10f687a17ed22f37681fd5944a5eb1f7228
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-03 12:26:14 +1200

    Update MainMenuUI.ts
    Created class definition

--- src/ui/uiFacade/MainMenuUI.ts
+++ src/ui/uiFacade/MainMenuUI.ts
@@ -1,3 +1,12 @@
-class MainMenuUI {
-    
+import { UIMenu } from './UIMenu';
+import { UILogic } from '../uiLogic/UILogic';
+export class MainMenuUI extends UIMenu {
+ 
+    constructor(logic : UILogic) {
+        super(logic);
+    }
+
+    public welcome() {
+
+    }
 }
