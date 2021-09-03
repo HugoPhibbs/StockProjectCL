0e8aff10b60391367466386c6d0f1b57d4b4a304
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-03 18:07:39 +1200

    Update UILogic.ts
    Added method to throw an exception for an invalid command.
    
    Fixed small bug with handling an option choice

--- src/ui/uiLogic/UILogic.ts
+++ src/ui/uiLogic/UILogic.ts
@@ -1,8 +1,13 @@
+/**
+ * Abstract class to handle controlling of the cmd line ui. 
+ * <p>
+ * Implementations are not intended to do any direct printing onto the command line, only take handle input and requests from a user directly related to the command line
+ */
 export abstract class UILogic {
 
-    constructor() {
-    }
-
+    /**
+     * Method to start a cmd line ui for a particular menu
+     */
     public abstract start() : void;
 
     /**
@@ -12,10 +17,21 @@ export abstract class UILogic {
      * A approach with having a command associated with an option, ensures that the ordering of options is unimportant.
      * 
      * @param options string[][] nested array containing the message of a particular option, along with the command that corresponds to this option
-     * @param chosenOption 
-     * @returns 
+     * @param chosenOption number for the option that a user has chosen. 
+     * @returns string for the command that a user has chosen
      */
     protected handleOptionChoice(options : string[][], chosenOption : number) : string {
-        return options[chosenOption][1];
+        return options[chosenOption-1][1];
+    }
+
+    /**
+     * Handles an unknown command within the application
+     * <p>
+     * Likely to be only called when there is a bug in the application
+     * 
+     * @param command string for an unknown command that was called
+     */
+    public unknownCommand(command : string) : void {
+        throw new Error(`IA Exception, ${command} is an unknown command!`);
     }
 }
