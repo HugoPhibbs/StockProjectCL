fb5108ef9906d85961d52a7e526511cf546f92ae
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-03 18:02:42 +1200

    Update AppEnvironment.ts
    Commented out rest part, wasnt working with serialization.
    
    Added attribute for Portfolio manager

--- src/coreClasses/coreObjects/AppEnvironment.ts
+++ src/coreClasses/coreObjects/AppEnvironment.ts
@@ -1,23 +1,31 @@
 
 import { User } from "./User";
+import { PortfolioManager } from './PorfolioManager';
 
 export class AppEnvironment {
 
-    private _rest;
+    // private _rest;
     private _user : User;
+
+    private _portfolioManager : PortfolioManager
 
     constructor(rest: any, user : User){
         // IRestClient doesn't work!
-        this._rest = rest;
+        //this._rest = rest;
         this._user = user;
+        this._portfolioManager = new PortfolioManager();
     }
 
-    get rest(){
-        return this._rest;
-    }
+    //get rest(){
+        //return this._rest;
+    //}
 
-    get user() {
+    get user() : User{
         return this._user;
+    }
+
+    get portfolioManager() : PortfolioManager{
+        return this._portfolioManager;
     }
 
 }
