d6a572e9ada44ef0f707510979ab0efc73fa1385
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-04 21:44:50 +1200

    Update UIMenu.ts
    Moved methods around.
    
    Added function to style prompts, automation is king

--- src/ui/uiFacade/UIMenu.ts
+++ src/ui/uiFacade/UIMenu.ts
@@ -29,6 +29,7 @@ export abstract class UIMenu {
      * @returns string that is entered, will be valid
      */
     public static inputStrAndCheck(prompt : string, requirements : string, checkIsValidFunction : (str : string) => boolean) : string {
+        prompt = this.stylePrompt(prompt);
         let input : string = pr(prompt);
         // TODO have a way to exit entering a screen, eg enter BACK to back out of this
         while (!checkIsValidFunction(input)) {
@@ -74,40 +75,6 @@ export abstract class UIMenu {
     }
 
     /**
-     * Prints options that a user can select that are based on their numbering
-     * 
-     * @param numOptions number of options that a user can select. Must be an integer, and 
-     * @param options string[][] nested array for the options that a user can select. 
-     */
-     public static printOptions(options : string[][]) {
-        let numOptions = options.length;
-        // TODO, auto extract option messages before this!
-        for (let i = 0; i < numOptions; i++){
-            this.print(`${i+1}. ${options[i][0]}`);
-        }
-    }
-
-    /**
-     * Asks a user to input a number based on a prompt. If the number entered isn't valid, then user is notified, and is asked to enter a number until it is valid. 
-     * <p>
-     * Keeps requirements separate so a user can enter numbers that dont necessarily reflect a chosen option, for example, entering a share price of a stock
-     * 
-     * @param requirements string for a message to tell a user the requirements of an inputted number
-     * @param checkIsValidFunction Function to check if the inputted number is valid
-     * @returns number that is entered, will be valid
-     */
-    private static inputNumAndCheck(requirements : string, checkIsValidFunction : (num:number) => boolean) : number {
-        let prompt = "Option number: ";
-        let input : number = pr(prompt);
-        // Need to check number is actually a number
-        while (!checkIsValidFunction(input)) {
-            this.print(requirements);
-            input = pr(prompt);
-        }
-        return input;
-    }
-
-    /**
      * Returns a string for the requirements of an entered option number
      * 
      * @param numOptions number for the number of options that a user can select
@@ -142,18 +109,64 @@ export abstract class UIMenu {
     /**
      * Handles process of getting an input from a user to enter an option
      * 
-     * @param prompt string for a prompt message to ask a user to select an option
+     * @param message string for a message to ask a user to select an option. Basically describing what this interaction entails
      * @param options string[][] nested array for the options that a user can select
      * @returns number corresponding to the option that a user has selected
      */
-    public static enterOption(prompt: string, options : string[][]) : number {
+    public static inputOption(message: string, options : string[][]) : number {
         let requirements : string = this.optionNumRequirements(options.length);
-        this.print(prompt);
+        this.print(message);
         this.printOptions(options)
         let optionNumIsValidFuncHandle : (num : number) => boolean = this.optionNumIsValidFuncHandle(options);
         return this.inputNumAndCheck(requirements, optionNumIsValidFuncHandle);
     }
+
+    /**
+     * Asks a user to input a number based on a prompt. If the number entered isn't valid, then user is notified, and is asked to enter a number until it is valid. 
+     * <p>
+     * Keeps requirements separate so a user can enter numbers that dont necessarily reflect a chosen option, for example, entering a share price of a stock
+     * 
+     * @param requirements string for a message to tell a user the requirements of an inputted number
+     * @param checkIsValidFunction Function to check if the inputted number is valid
+     * @returns number that is entered, will be valid
+     */
+    private static inputNumAndCheck(requirements : string, checkIsValidFunction : (num:number) => boolean) : number {
+        let prompt : string = this.stylePrompt("Enter option option number");
+        let input : number = pr(prompt);
+        // Need to check number is actually a number
+        while (!checkIsValidFunction(input)) {
+            this.print(requirements);
+            input = pr(prompt);
+        }
+        return input;
+    }
     
+
+    /**
+     * Prints options that a user can select that are based on their numbering
+     * 
+     * @param numOptions number of options that a user can select. Must be an integer, and 
+     * @param options string[][] nested array for the options that a user can select. 
+     */
+    private static printOptions(options : string[][]) {
+        let numOptions = options.length;
+        // TODO, auto extract option messages before this!
+        for (let i = 0; i < numOptions; i++){
+            this.print(`${i+1}. ${options[i][0]}`);
+        }
+    }
+
+    /**
+     * Styles a prompt that is printed to a user. May seem unnecessary to put such a simple action into a function, but this insures
+     * minimal refactoring if you wish to change the way that prompts look. 
+     *
+     * @param prompt prompt asking a user to input a number or string etc
+     * @returns prompt styled as describe
+     */
+    private static stylePrompt(prompt: string) : string {
+        let style : string = ": ";
+        return prompt.concat(style);
+    }
 
     /**
      * Prints a message to a user. Extrapolated to minimize refactoring if the printing out method changed
