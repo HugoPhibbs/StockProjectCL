490bc6bfde3faa95513a824f41d6bc5b54407974
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-05 12:54:37 +1200

    Update CheckInput.ts

--- src/coreClasses/coreLogic/CheckInput.ts
+++ src/coreClasses/coreLogic/CheckInput.ts
@@ -7,7 +7,10 @@ export class CheckInput {
      * @returns boolean if inputted name is valid
      */
     public static nameIsValid(nameToCheck : string) : boolean {
-        if (nameToCheck.length == 0 || nameToCheck.length > 10){
+        if (nameToCheck == undefined){
+            return false;
+        }
+        else if (nameToCheck.length == 0 || nameToCheck.length > 10){
             return false;
         }
         else if (/^[a-zA-Z0-9]*$/.test(nameToCheck) == false) {
@@ -22,6 +25,6 @@ export class CheckInput {
      * Getter method for valid name requirements
      */
     static get nameRequirements() : string {
-        return "Name must have length between 1 and 10 (inclusive)\nAnd may only contain letters and numbers";
+        return "name must have length between 1 and 10 (inclusive) and may only contain letters and numbers";
     }
 }
