851fb57f06444d495179f5181a43da631b6c8466
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-06 21:00:40 +1200

    Update StockLogic.ts
    Updated so most of the methods work, not sure really if they work well tho!!!

--- src/coreClasses/coreLogic/StockLogic.ts
+++ src/coreClasses/coreLogic/StockLogic.ts
@@ -1,4 +1,6 @@
 import { AnyTxtRecord } from "dns";
+import { Holding } from "../coreObjects/Holding";
+import { PassThrough } from 'stream';
 
 /**
  * Class to do any logic to do with the polygon.io api\
@@ -30,8 +32,8 @@ export class StockLogic {
      */
     constructor() {
         let api_key : string = process.env.POLYGON_API_KEY;
-        this._rest = require("@polygon.io/client-js").restClient(api_key);
-        this._ref = require("@polygon.io/client-js").referenceClient(api_key);
+        this._rest = require("@polygon.io/client-js").restClient("s1pd4MlVenZGJWPP8p1mN3BUpO7TVYZq");
+        this._ref = require("@polygon.io/client-js").referenceClient("s1pd4MlVenZGJWPP8p1mN3BUpO7TVYZq");
         this._deAsync = require("deasync");
     }
 
@@ -54,8 +56,7 @@ export class StockLogic {
         let result : boolean = data.results != undefined
         return result;
     }
-
-
+    
     /**
      * Finds the last closing price for a symbol
      * 
@@ -85,9 +86,78 @@ export class StockLogic {
         }
     }
 
-    public totalReturn(symbol : string, buyPrice : number) : number{
+    /**
+     * Finds the total return of a holding expresses in a dollar amount
+     * 
+     * @param holding Holding object to find the actual return for
+     * @returns number expressing the v
+     */
+    public totalReturnActual(holding : Holding) : number {
+        // Finds the total return from a holding of stock (actual gain)
+        let buyPrice : number = holding.buyPrice;
+        let symbol : string = holding.symbol;
+        let shares : number = holding.shares;
+        let totalReturn : number = shares *  this.totalGainActual(symbol, buyPrice);
+        return totalReturn;
+    }
+
+    /**
+     * Calculates the percentage gain of a Holding
+     * 
+     * @param holding Holding object for the percentage gain to be calculated for
+     * @returns string expressing the percentage gain of a Holding
+     */
+    public totalReturnPerc(holding : Holding) : string {
+        // Finds the total return from a holding of stock (in percentage)
+        let buyPriceTotal = holding.buyPrice * holding.shares;
+        return this.calcPercentage(buyPriceTotal, this.totalReturnActual(holding));
+    }
+
+
+    /**
+     * Calculates a percentage given a numerator and denominator
+     * 
+     * @param numerator number expressing the top of the fraction
+     * @param denominator number expressing the bottom of the fraction
+     * @returns string expressing the percentage proportion between numerator and the denominator. 
+     */
+    private calcPercentage(numerator : number, denominator : number) : string{
+        return `${(numerator / denominator) * 100}%`
+    }
+    
+    /**
+     * Finds the actual gain of a stock with inputted symbol
+     * 
+     * @param symbol strimg for the symbol of a stock
+     * @param buyPrice price that a stock was bought at (per share)
+     * @returns number expressing the total gain of a stock
+     */
+    private totalGainActual(symbol : string, buyPrice : number) : number{
         // finds the total return for a symbol
-        return 1;
+        let closingPrice : number = this.previousClose(symbol);
+        let totalGain = closingPrice - buyPrice;
+        return totalGain;
+    }
+
+    /**
+     * Returns the requirements for a valid ticker symbol
+     * 
+     * @returns string describing valid ticker symbol name requirements
+     */
+    public static validSymbolRequirements() : string {
+        return "Ticker symbol must match a stock that Polygon.io has data for, see online for details";
+    }
+
+
+    /**
+     * Returns the total return of a symbol expressed as a symbol
+     * 
+     * @param symbol string for the symbol of a stock/ticker 
+     * @param buyPrice number for the buyPrice (per share) of a stock
+     * @returns string expressing the percentage return of a stock
+     */
+    private totalGainPerc(symbol : string, buyPrice : number) : string {
+        return this.calcPercentage(buyPrice, this.totalGainActual(symbol, buyPrice))
     }
 }
 
