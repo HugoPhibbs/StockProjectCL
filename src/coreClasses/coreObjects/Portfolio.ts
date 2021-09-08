46ea5494a306bc93096fa2398a806aedecb51df3
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-08 20:18:30 +1200

    added method to convert holdings to a table

--- src/coreClasses/coreObjects/Portfolio.ts
+++ src/coreClasses/coreObjects/Portfolio.ts
@@ -1,58 +1,68 @@
-import { Holding } from "./Holding";
-import { PortfolioManager } from "./PorfolioManager";
-import { PassThrough } from 'stream';
-import { CheckInput } from "../coreLogic/CheckInput";
+import {Holding} from "./Holding";
 
 /*
 Class to represent a collection of holdings
 */
 export class Portfolio {
 
-    private _holdings : Holding[] = [];
-    private _name : string;
+    private _holdings: Holding[] = [];
+    private _name: string;
 
-    constructor(name : string){
+    constructor(name: string) {
         this._name = name;
     }
 
-    public addHolding(holding : Holding) {
+    public addHolding(holding: Holding) {
         this._holdings.push(holding);
     }
 
-    public totalValue() : number{
+    public totalValue(): number {
         // Returns the total value of a portfolio
-        return 1;
+        return -2;
     }
 
-    public dailyChange(percentage : boolean) : number {
+    public dailyChange(percentage: boolean): number {
         // Returns the daily change of a portfolio
         return 1;
     }
 
-    public totalChange(percentage: boolean) : number{
+    public totalChange(percentage: boolean): number {
         // Returns the total change in portfolio
         return 1
     }
 
-    public equals(obj : any) : boolean{
-        if (typeof obj != typeof this){ 
-            return false;
+    /**
+     * Returns an array of objects describing holdings for this portfolio
+     *
+     * @returns object as described
+     */
+    public holdingsToTable(): [{ Symbol: string; Shares: number } & { "Total return (%)": number; "Total return ($)": string; "Daily return (%)": string; "Daily return ($)": number }] {
+        let table = [];
+        for (let i = 0; i < this._holdings.length; i++) {
+            table.push(this._holdings[i].toDict())
         }
-        else {
+        // @ts-ignore
+        return table;
+    }
+
+    public equals(obj: any): boolean {
+        if (obj == null || typeof obj != typeof this) {
+            return false;
+        } else {
             obj = (obj as Portfolio);
             return (obj.name == this.name);
         }
     }
 
-    get name() : string {
+    get name(): string {
         return this._name;
     }
 
-    get holdings() : Holding[] {
+    get holdings(): Holding[] {
         return this._holdings;
     }
-    
-    set name(name : string) {
+
+    set name(name: string) {
         this._name = name;
     }
 }
