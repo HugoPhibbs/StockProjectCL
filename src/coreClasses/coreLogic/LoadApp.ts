b8baf3ea7d951edfe3b25f88108959648b79766b
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-09-08 20:17:38 +1200

    doc

--- src/coreClasses/coreLogic/LoadApp.ts
+++ src/coreClasses/coreLogic/LoadApp.ts
@@ -1,73 +1,109 @@
-import { AppEnvironment } from '../coreObjects/AppEnvironment';
-import { LoadAppUI } from '../../ui/uiLogic/LoadAppUI';
-import { User } from '../coreObjects/User';
-import { MainMenuUI } from '../../ui/uiLogic/MainMenuUI';
-import { assert } from 'console';
-const ps = require("prompt-sync");
+import {AppEnvironment} from '../coreObjects/AppEnvironment';
+import {LoadAppUI} from '../../ui/uiLogic/LoadAppUI';
+import {User} from '../coreObjects/User';
+import {MainMenuUI} from '../../ui/uiLogic/MainMenuUI';
+import assert = require('assert');
+
 const ess = require('esserializer');
-var fs = require('fs');
+const fs = require('fs');
 
-const pr = ps();
-
+/**
+ * Class to manage the loading of the application
+ */
 export class LoadApp {
 
+    /**
+     * App Environment object for this application.
+     * Either created or loaded from a file by this class
+     *
+     * @private
+     */
     private _appEnvironment!: AppEnvironment;
 
-    private _loadOptions : string[][] = [
+    /**
+     * Options that a user can choose when starting the app
+     * @private
+     */
+    private _loadOptions: string[][] = [
         ["Create a save", "CREATE"],
         ["Load a save", "LOAD"]
     ];
 
-    private _workingDirectory : string | undefined;
+    /**
+     * String for the working directory that this application is saved to
+     * @private
+     */
+    private _workingDirectory: string | undefined;
 
-    static checkIsValidFunction: any;
-
-    constructor(){
+    /**
+     * Constructor
+     */
+    constructor() {
     }
 
-    public start() : void {
-        let loadAppUILogic : LoadAppUI = new LoadAppUI(this);
+    /**
+     * Starts the application
+     */
+    public start(): void {
+        let loadAppUILogic: LoadAppUI = new LoadAppUI(this);
         loadAppUILogic.start();
         this.createMainMenu();
     }
 
-    public createMainMenu() : void{
+    /**
+     * Creates the main menu for this app
+     */
+    public createMainMenu(): void {
         assert(this._appEnvironment != undefined);
-        let mainMenuUILogic : MainMenuUI = new MainMenuUI(this._appEnvironment);
+        let mainMenuUILogic: MainMenuUI = new MainMenuUI(this._appEnvironment);
         mainMenuUILogic.start()
     }
 
-    private loadSave(directory : string) : boolean {
+    /**
+     * Loads a save from a file
+     *
+     * @param directory string for the directory that a file is saved at
+     * @private
+     * @returns boolean if a save file was loaded or not
+     */
+    private loadSave(directory: string): boolean {
         return false;
     }
 
     /**
      * Handles creating a save file for the application. This is when a user wants to start a fresh
      * version of the application, without any progress
-     * 
+     *
      * @param userName string for the name of a user for application progress to be saved under
      * @returns boolean if a save was created
      */
-    public createSave(userName : string) : boolean  {
+    public createSave(userName: string): boolean {
         let user = new User(userName);
         this._appEnvironment = new AppEnvironment(this, user);
         let serializedJSON = ess.serialize(this._appEnvironment, User);
-        let sessionDirectory : string = this.sessionDirectory(userName)
+        let sessionDirectory: string = this.sessionDirectory(userName)
         fs.writeFileSync(sessionDirectory, serializedJSON, 'utf-8')
         return true;
     }
 
-    private sessionDirectory(userName : string ) {
+    /**
+     * Finds the directory that an app session will be saved at
+     *
+     * @param userName string for the name of the user loading the application
+     * @returns string as described
+     * @private
+     */
+    private sessionDirectory(userName: string): string {
         return `${this._workingDirectory}\\app_saves\\${userName}_save.json`;
     }
 
     /**
      * Checks if an inputted directory is valid
-     * 
+     *
      * @param directory string for a directory to be checked if it is valid or not
      * @returns boolean if inputted directory is valid or not
      */
-    public static directoryIsValid(directory : string) : boolean {
+    public static directoryIsValid(directory: string): boolean {
         // Returns if a directory is valid or not
         // TODO implement!
         return true;
@@ -83,22 +119,28 @@ export class LoadApp {
     public handleLoadCommand(loadCommand : string, userName: string) {
         if (loadCommand === "CREATE"){
             this.createSave(userName);
-        }
-        else if (loadCommand === "LOAD") {
+        } else if (loadCommand === "LOAD") {
             this.loadSave(userName);
-        }
-        else {
+        } else {
             throw new Error(`IA exception, load command "${loadCommand}" not valid!`);
         }
     }
 
-
-
-    get loadOptions() : string[][] {
+    /**
+     * Getter method for the options that a user can choose when loading the application
+     *
+     * @returns nested array containing the description of options, along with their corresponding commands
+     */
+    get loadOptions(): string[][] {
         return this._loadOptions;
     }
 
-    set workingDirectory(workingDirectory : string) {
+    /**
+     * Setter method for the working directory of this class
+     *
+     * @param workingDirectory string for teh working directory of this class and app
+     */
+    set workingDirectory(workingDirectory: string) {
         this._workingDirectory = workingDirectory;
     }
 
